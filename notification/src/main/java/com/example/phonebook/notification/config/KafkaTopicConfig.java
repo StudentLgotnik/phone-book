44554846d74fd252870342d0a55ca40cc6015ef8
Maksym Kazarin <kazzarin99@gmail.com>
2021-05-10 13:07:39 +0300

    Topic configuration was added to notification and statistic service

---
+++ notification/src/main/java/com/example/phonebook/notification/config/KafkaTopicConfig.java
@@ -0,0 +1,33 @@
+package com.example.phonebook.notification.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    private static final int DEFAULT_TOPIC_PARTITIONS = 3;
+    private static final int DEFAULT_TOPIC_REPLICATIONS = 1;
+
+    @Value(value = "${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic phoneBookTopic() {
+        return new NewTopic("phonebook", DEFAULT_TOPIC_PARTITIONS, (short) DEFAULT_TOPIC_REPLICATIONS);
+    }
+}
